e56595ee1348edc64d57c58367ba2b4a04c42604
gpittarelli <tf2stadium@gjp.cc>
2016-01-07 17:58:04 -0500

    Add server_cvar message handling
    Note: Requires ParseMsg Data field to be cast to PlayerData or CvarData

--- chatMessage_test.go
+++ chatMessage_test.go
@@ -15,6 +15,7 @@ var logs []string = []string{
 	`"Sk1LL0<2><[U:1:198288660]><Red>" joined team "Blue"`,
 	`"Sk1LL0<2><[U:1:198288660]><Blue>" changed role to "medic"`,
 	`World triggered "Game_Over" reason "Reached Win Difference Limit"`,
+	`server_cvar: "sv_password" "***PROTECTED***"`,
 }
 
 func TestParse(t *testing.T) {
@@ -27,57 +28,75 @@ func TestParse(t *testing.T) {
 		case 0:
 			assert.Equal(t, m.Type, PlayerChangedTeam)
 
-			assert.Equal(t, m.Data.Team, "Unassigned")
-			assert.Equal(t, m.Data.NewTeam, "Red")
+			playerData := m.Data.(PlayerData)
+			assert.Equal(t, playerData.Team, "Unassigned")
+			assert.Equal(t, playerData.NewTeam, "Red")
 
-			assert.Equal(t, m.Data.Username, "Sk1LL0")
-			assert.Equal(t, m.Data.UserId, "2")
-			assert.Equal(t, m.Data.SteamId, "[U:1:198288660]")
+			assert.Equal(t, playerData.Username, "Sk1LL0")
+			assert.Equal(t, playerData.UserId, "2")
+			assert.Equal(t, playerData.SteamId, "[U:1:198288660]")
 
 			// 1 = changed class
 		case 1:
 			assert.Equal(t, m.Type, PlayerChangedClass)
-			assert.Equal(t, m.Data.Class, "scout")
+
+			playerData := m.Data.(PlayerData)
+			assert.Equal(t, playerData.Class, "scout")
 
 			// 2 = changed class
 		case 2:
 			assert.Equal(t, m.Type, PlayerChangedClass)
-			assert.Equal(t, m.Data.Class, "soldier")
+
+			playerData := m.Data.(PlayerData)
+			assert.Equal(t, playerData.Class, "soldier")
 
 			// 3 = global message
 		case 3:
 			assert.Equal(t, m.Type, PlayerGlobalMessage)
 
-			assert.Equal(t, m.Data.Team, "Red")
-			assert.Equal(t, m.Data.Text, "hello gringos")
+			playerData := m.Data.(PlayerData)
+			assert.Equal(t, playerData.Team, "Red")
+			assert.Equal(t, playerData.Text, "hello gringos")
 
 			// 4 = team message
 		case 4:
 			assert.Equal(t, m.Type, PlayerTeamMessage)
-			assert.Equal(t, m.Data.Text, "ufo porno")
+			playerData := m.Data.(PlayerData)
+			assert.Equal(t, playerData.Text, "ufo porno")
 
 			// 5 = changed class
 		case 5:
 			assert.Equal(t, m.Type, PlayerChangedClass)
-			assert.Equal(t, m.Data.Class, "sniper")
+			playerData := m.Data.(PlayerData)
+			assert.Equal(t, playerData.Class, "sniper")
 
 			// 6 = changed team
 		case 6:
 			assert.Equal(t, m.Type, PlayerChangedTeam)
 
-			assert.Equal(t, m.Data.Team, "Red")
-			assert.Equal(t, m.Data.NewTeam, "Blue")
+			playerData := m.Data.(PlayerData)
+			assert.Equal(t, playerData.Team, "Red")
+			assert.Equal(t, playerData.NewTeam, "Blue")
 
 			// 7 = changed class
 		case 7:
 			assert.Equal(t, m.Type, PlayerChangedClass)
 
-			assert.Equal(t, m.Data.Team, "Blue")
-			assert.Equal(t, m.Data.Class, "medic")
+			playerData := m.Data.(PlayerData)
+			assert.Equal(t, playerData.Team, "Blue")
+			assert.Equal(t, playerData.Class, "medic")
 
 			// 8 = game over
 		case 8:
 			assert.Equal(t, m.Type, WorldGameOver)
+
+			// 9 = server cvar
+		case 9:
+			assert.Equal(t, m.Type, ServerCvar)
+
+			cvarData := m.Data.(CvarData)
+			assert.Equal(t, cvarData.Variable, "sv_password")
+			assert.Equal(t, cvarData.Value, "***PROTECTED***")
 		}
 	}
 }
