227d79af0f6d2a8517e71bedff301e36bd3b0b2e
Vibhav Pant <vibhavp@gmail.com>
2016-02-19 00:25:46 +0530

    Add more events.

--- chatMessage.go
+++ chatMessage.go
@@ -3,6 +3,7 @@ package TF2RconWrapper
 import (
 	"errors"
 	"regexp"
+	"strconv"
 	"time"
 )
 
@@ -10,6 +11,7 @@ const (
 	// "Username<userId><steamId><Team>"
 	// "1<2><3><4>" <- regex group
 	logLineStart = `^"(.*)<(\d+)><(\[U:1:\d+\])><(\w+)>" `
+	player       = `"(.*)<(\d+)><(\[U:1:\d+\])><(\w+)>"`
 	// "5" <- regex group
 	logLineEnd = ` "(.*)"`
 
@@ -18,18 +20,59 @@ const (
 
 // regexes used in the parser
 var (
-	rPlayerGlobalMessage = regexp.MustCompile(logLineStart + `say` + logLineEnd)
-	rPlayerChangedClass  = regexp.MustCompile(logLineStart + `changed role to` + logLineEnd)
-	rPlayerTeamMessage   = regexp.MustCompile(logLineStart + `say_team` + logLineEnd)
-	rPlayerChangedTeam   = regexp.MustCompile(logLineStart + `joined team` + logLineEnd)
+	rPlayerGlobalMessage  = regexp.MustCompile(logLineStart + `say` + logLineEnd)
+	rPlayerChangedClass   = regexp.MustCompile(logLineStart + `changed role to` + logLineEnd)
+	rPlayerTeamMessage    = regexp.MustCompile(logLineStart + `say_team` + logLineEnd)
+	rPlayerChangedTeam    = regexp.MustCompile(logLineStart + `joined team` + logLineEnd)
+	rPlayerPickedUp       = regexp.MustCompile(logLineStart + `picked up item "(\w+)"(?: \(healing "(\d+)"\))*`)
+	rPlayerSpawned        = regexp.MustCompile(logLineStart + `spawned as "(\w+)"`)
+	rPlayerKilled         = regexp.MustCompile(logLineStart + `killed ` + player + ` with "(\w+)"(?: \(customkill "(\w+)"\)){0,1} \(attacker_position (".+")\) \(victim_position (".+")\)`)
+	rPlayerDamage         = regexp.MustCompile(logLineStart + `triggered "damage" against ` + player + ` \(damage "(\d+)"\)(?: \(realdamage "\d+"\)){0,1} \(weapon "(\w+)"\)( \(airshot "\d+"\)){0,1}`)
+	rPlayerHeal           = regexp.MustCompile(logLineStart + `triggered "healed" against ` + player + ` \(healing "(\d+)"\)`)
+	rPlayerKilledMedic    = regexp.MustCompile(logLineStart + `triggered "medic_death" against ` + player + ` \(healing "(\d+)"\) \(ubercharge "(\d+)"\)`)
+	rPlayerUberFinished   = regexp.MustCompile(logLineStart + `triggered "empty_uber"`)
+	rPlayerBlockedCapture = regexp.MustCompile(logLineStart + `triggered "captureblocked" \(?:cp "(\d+)"\) \(cpname ("#\w+")\) \(position "(.+)"\)`)
+	rPlayerConnected      = regexp.MustCompile(logLineStartSpec + `connected, address "\d+.\d+.\d+.\d+\:\d+"`)
+	rPlayerDisconnected   = regexp.MustCompile(logLineStartSpec + `disconnected \(reason "(.*)"\)`)
 
-	//Global Messages
-	rPlayerConnected    = regexp.MustCompile(logLineStartSpec + `connected, address "\d+.\d+.\d+.\d+\:\d+"`)
-	rPlayerDisconnected = regexp.MustCompile(logLineStartSpec + `disconnected \(reason "(.*)"\)`)
-	rGameOver           = regexp.MustCompile(`^World triggered "Game_Over" reason "(.*)"`)
-	rServerCvar         = regexp.MustCompile(`^server_cvar: "(.*)" "(.*)"`)
+	//Team events
+	rTeamPointCapture = regexp.MustCompile(`^Team "(Red|Blue)" triggered "pointcaptured" \(cp "(\d+)"\) \(cpname ("#\w+")\)`)
+	rTeamScoreUpdate  = regexp.MustCompile(`^Team "(Red|Blue)" current score "(\d+)" with "\d+" players`)
+
+	//World events
+	rGameOver   = regexp.MustCompile(`^World triggered "Game_Over" reason "(.*)"`)
+	rRoundWin   = regexp.MustCompile(`^World triggered "Round_Win" \(winner "(Red|Blue)"\)`)
+	rRoundStart = regexp.MustCompile(`^World triggered "Round_Start"`)
+	rServerCvar = regexp.MustCompile(`^server_cvar: "(.*)" "(.*)"`)
 
 	rLogFiledClosed = regexp.MustCompile("^Log file closed.")
+)
+
+const (
+	PlayerGlobalMessage = iota
+	PlayerTeamMessage
+	PlayerChangedClass
+	PlayerChangedTeam
+	PlayerPickedUpItem
+	PlayerSpawned
+	PlayerKilled
+	PlayerDamaged
+	PlayerHealed
+	PlayerKilledMedic
+	PlayerUberFinished
+	PlayerBlockedCapture
+	PlayerConnected
+	PlayerDisconnected
+
+	TeamPointCapture
+	TeamScoreUpdate
+
+	WorldGameOver
+	WorldRoundWin
+	WorldRoundStart
+	ServerCvar
+
+	LogFileClosed
 )
 
 //LogMessage represents a log message in a TF2 server, and contains a timestamp
@@ -39,6 +82,43 @@ type LogMessage struct {
 	Timestamp time.Time
 	Message   string
 	Parsed    ParsedMsg
+}
+
+type TeamData struct {
+	Team    string
+	Trigger string
+	CP      string
+	CPName  string
+}
+
+type ItemPickup struct {
+	PlayerData PlayerData
+	Item       string
+	Healing    int
+}
+
+type PlayerTrigger struct {
+	//player1 triggered "Foo" against player2
+	Player1 PlayerData
+	Player2 PlayerData
+}
+
+type PlayerKill struct {
+	PlayerTrigger
+	Weapon     string
+	CustomKill string
+}
+
+type PlayerDamage struct {
+	PlayerTrigger
+	Damage  int
+	Weapon  string
+	Airshot bool
+}
+
+type PlayerHeal struct {
+	PlayerTrigger
+	Healed int // health gained
 }
 
 //When a player says something in the global chat, or when they join the game
@@ -59,20 +139,6 @@ type CvarData struct {
 	Variable string
 	Value    string
 }
-
-const (
-	PlayerGlobalMessage = iota
-	PlayerTeamMessage
-	PlayerChangedClass
-	PlayerChangedTeam
-
-	WorldPlayerConnected
-	WorldPlayerDisconnected
-	WorldGameOver
-	ServerCvar
-
-	LogFileClosed = iota
-)
 
 type ParsedMsg struct {
 	Type int
@@ -121,76 +187,173 @@ func parse(text string) LogMessage {
 	return LogMessage{timeObj, text, ParseLine(message)}
 }
 
+func getPlayerData(matches []string, from int, includeTeam bool) PlayerData {
+	d := PlayerData{
+		Username: matches[from+0],
+		UserId:   matches[from+1],
+		SteamId:  matches[from+2],
+	}
+
+	if includeTeam {
+		d.Team = matches[from+3]
+	}
+
+	return d
+}
+
 func ParseLine(message string) ParsedMsg {
 	r := ParsedMsg{Type: -1}
-	var m []string
 
-	isPlayerMessage := false
-	playerData := PlayerData{}
 	switch {
 	case rPlayerGlobalMessage.MatchString(message):
-		m = rPlayerGlobalMessage.FindStringSubmatch(message)
+		m := rPlayerGlobalMessage.FindStringSubmatch(message)
 
-		isPlayerMessage = true
+		playerData := getPlayerData(m, 1, true)
 		playerData.Text = m[5]
+		r.Data = playerData
 		r.Type = PlayerGlobalMessage
 
 	case rPlayerTeamMessage.MatchString(message):
-		m = rPlayerTeamMessage.FindStringSubmatch(message)
+		m := rPlayerTeamMessage.FindStringSubmatch(message)
 
-		isPlayerMessage = true
+		playerData := getPlayerData(m, 1, true)
 		playerData.Text = m[5]
+		r.Data = playerData
 		r.Type = PlayerTeamMessage
 
 	case rPlayerChangedClass.MatchString(message):
-		m = rPlayerChangedClass.FindStringSubmatch(message)
+		m := rPlayerChangedClass.FindStringSubmatch(message)
 
-		isPlayerMessage = true
+		playerData := getPlayerData(m, 1, true)
 		playerData.Class = m[5]
+		r.Data = playerData
 		r.Type = PlayerChangedClass
 
 	case rPlayerChangedTeam.MatchString(message):
-		m = rPlayerChangedTeam.FindStringSubmatch(message)
+		m := rPlayerChangedTeam.FindStringSubmatch(message)
 
-		isPlayerMessage = true
+		playerData := getPlayerData(m, 1, true)
 		playerData.NewTeam = m[5]
+		r.Data = playerData
 		r.Type = PlayerChangedTeam
 
-	case rPlayerConnected.MatchString(message):
-		m = rPlayerConnected.FindStringSubmatch(message)
+	case rPlayerPickedUp.MatchString(message):
+		m := rPlayerPickedUp.FindStringSubmatch(message)
 
-		isPlayerMessage = true
-		r.Type = WorldPlayerConnected
+		playerData := getPlayerData(m, 1, true)
+		pickup := ItemPickup{
+			PlayerData: playerData,
+			Item:       m[5],
+		}
+
+		if len(m) == 7 {
+			pickup.Healing, _ = strconv.Atoi(m[6])
+		}
+
+		r.Data = pickup
+		r.Type = PlayerPickedUpItem
+
+	case rPlayerKilled.MatchString(message):
+		m := rPlayerKilled.FindStringSubmatch(message)
+
+		kill := PlayerKill{
+			PlayerTrigger: PlayerTrigger{
+				Player1: getPlayerData(m, 1, true),
+				Player2: getPlayerData(m, 5, true),
+			},
+		}
+
+		kill.Weapon = m[9]
+		if len(m) > 10 {
+			kill.CustomKill = m[10]
+		}
+
+		r.Data = kill
+		r.Type = PlayerKilled
+
+	case rPlayerDamage.MatchString(message):
+		m := rPlayerDamage.FindStringSubmatch(message)
+
+		dmg, _ := strconv.Atoi(m[9])
+		damage := PlayerDamage{
+			PlayerTrigger: PlayerTrigger{
+				Player1: getPlayerData(m, 1, true), // ends at m[4]
+				Player2: getPlayerData(m, 5, true), // ends at m[8]
+			},
+
+			Damage:  dmg,
+			Weapon:  m[10],
+			Airshot: len(m) == 12,
+		}
+		r.Data = damage
+		r.Type = PlayerDamaged
+
+	case rPlayerHeal.MatchString(message):
+		m := rPlayerHeal.FindStringSubmatch(message)
+
+		healing, _ := strconv.Atoi(m[9])
+		heal := PlayerHeal{
+			PlayerTrigger: PlayerTrigger{
+				Player1: getPlayerData(m, 1, true), // end at m[4]
+				Player2: getPlayerData(m, 5, true), // ends at m[8]
+			},
+			Healed: healing,
+		}
+		r.Data = heal
+		r.Type = PlayerHealed
+
+	case rPlayerKilledMedic.MatchString(message):
+		m := rPlayerKilledMedic.FindStringSubmatch(message)
+
+		r.Data = PlayerTrigger{
+			Player1: getPlayerData(m, 1, true),
+			Player2: getPlayerData(m, 5, true),
+		}
+		r.Type = PlayerKilledMedic
+
+	case rPlayerUberFinished.MatchString(message):
+		m := rPlayerUberFinished.FindStringSubmatch(message)
+
+		r.Data = getPlayerData(m, 1, true)
+		r.Type = PlayerUberFinished
+
+	case rPlayerBlockedCapture.MatchString(message):
+		m := rPlayerBlockedCapture.FindStringSubmatch(message)
+
+		r.Data = getPlayerData(m, 1, true)
+		r.Type = PlayerBlockedCapture
+
+	case rPlayerConnected.MatchString(message):
+		m := rPlayerConnected.FindStringSubmatch(message)
+
+		playerData := getPlayerData(m, 1, false)
+		r.Data = playerData
+		r.Type = PlayerConnected
 
 	case rPlayerDisconnected.MatchString(message):
-		m = rPlayerDisconnected.FindStringSubmatch(message)
+		m := rPlayerDisconnected.FindStringSubmatch(message)
 
-		isPlayerMessage = true
-		r.Type = WorldPlayerDisconnected
+		playerData := getPlayerData(m, 1, false)
+		r.Data = playerData
+		r.Type = PlayerDisconnected
 
 	// Non-Player Messages
 	case rGameOver.MatchString(message):
 		r.Type = WorldGameOver
 
+	case rRoundWin.MatchString(message):
+		m := rRoundWin.FindStringSubmatch(message)
+		r.Data = m[1]
+		r.Type = WorldRoundWin
+
 	case rServerCvar.MatchString(message):
-		m = rServerCvar.FindStringSubmatch(message)
+		m := rServerCvar.FindStringSubmatch(message)
 
 		r.Type = ServerCvar
 		r.Data = CvarData{Variable: m[1], Value: m[2]}
 
 	case rLogFiledClosed.MatchString(message):
 		r.Type = LogFileClosed
-	}
-
-	// fields used in all matches
-	if isPlayerMessage {
-		playerData.Username = m[1]
-		playerData.SteamId = m[3]
-		playerData.UserId = m[2]
-		if r.Type != WorldPlayerDisconnected {
-			playerData.Team = m[4]
-		}
-		r.Data = playerData
 	}
 
 	return r
--- chatMessage_test.go
+++ chatMessage_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 var logs = []string{
@@ -17,6 +18,15 @@ var logs = []string{
 	`"Sk1LL0<2><[U:1:198288660]><Blue>" changed role to "medic"`,
 	`World triggered "Game_Over" reason "Reached Win Difference Limit"`,
 	`server_cvar: "sv_password" "***PROTECTED***"`,
+	`"Sk1LL0<2><[U:1:198288660]><Blue>" picked up item "medkit_small" (healing "17")`,
+	`"Tedstur<9><[U:1:98355052]><Red>" killed "Lyreix | TF2Stadium.com<4><[U:1:56108026]><Blue>" with "scattergun" (attacker_position "-2310 -303 256") (victim_position "-2180 -474 256")`,
+	`"Tedstur<9><[U:1:98355052]><Red>" killed "Lyreix | TF2Stadium.com<4><[U:1:56108026]><Blue>" with "awper_hand" (customkill "headshot") (attacker_position "-673 1008 384") (victim_position "-754 802 391")`,
+	`"≫HarZe<3><[U:1:40572775]><Blue>" triggered "damage" against "beastie<5><[U:1:28701225]><Red>" (damage "100") (realdamage "88") (weapon "iron_bomber")`,
+	`"emkay lft<8><[U:1:64912509]><Red>" triggered "damage" against "≫HarZe<3><[U:1:40572775]><Blue>" (damage "43") (weapon "tf_projectile_rocket") (airshot "1")`,
+	`"Slappy™<11><[U:1:56973094]><Blue>" triggered "healed" against "mu<12><[U:1:33573908]><Blue>" (healing "61")`,
+	`"Lyreix | TF2Stadium.com<4><[U:1:56108026]><Blue>" triggered "medic_death" against "crab_f ring plz<6><[U:1:84999165]><Red>" (healing "802") (ubercharge "0")`,
+	`World triggered "Round_Win" (winner "Blue")`,
+	`"≫HarZe<3><[U:1:40572775]><>" connected, address "0.0.0.0:27005"`,
 }
 
 func TestParse(t *testing.T) {
@@ -24,7 +34,6 @@ func TestParse(t *testing.T) {
 		m := ParseLine(logs[i])
 
 		switch i {
-
 		// 0 = changed team
 		case 0:
 			assert.Equal(t, m.Type, PlayerChangedTeam)
@@ -39,21 +48,21 @@ func TestParse(t *testing.T) {
 
 			// 1 = changed class
 		case 1:
-			assert.Equal(t, m.Type, PlayerChangedClass)
+			require.Equal(t, m.Type, PlayerChangedClass)
 
 			playerData := m.Data.(PlayerData)
 			assert.Equal(t, playerData.Class, "scout")
 
 			// 2 = changed class
 		case 2:
-			assert.Equal(t, m.Type, PlayerChangedClass)
+			require.Equal(t, m.Type, PlayerChangedClass)
 
 			playerData := m.Data.(PlayerData)
 			assert.Equal(t, playerData.Class, "soldier")
 
 			// 3 = global message
 		case 3:
-			assert.Equal(t, m.Type, PlayerGlobalMessage)
+			require.Equal(t, m.Type, PlayerGlobalMessage)
 
 			playerData := m.Data.(PlayerData)
 			assert.Equal(t, playerData.Team, "Red")
@@ -61,19 +70,19 @@ func TestParse(t *testing.T) {
 
 			// 4 = team message
 		case 4:
-			assert.Equal(t, m.Type, PlayerTeamMessage)
+			require.Equal(t, m.Type, PlayerTeamMessage)
 			playerData := m.Data.(PlayerData)
 			assert.Equal(t, playerData.Text, "ufo porno")
 
 			// 5 = changed class
 		case 5:
-			assert.Equal(t, m.Type, PlayerChangedClass)
+			require.Equal(t, m.Type, PlayerChangedClass)
 			playerData := m.Data.(PlayerData)
 			assert.Equal(t, playerData.Class, "sniper")
 
 			// 6 = changed team
 		case 6:
-			assert.Equal(t, m.Type, PlayerChangedTeam)
+			require.Equal(t, m.Type, PlayerChangedTeam)
 
 			playerData := m.Data.(PlayerData)
 			assert.Equal(t, playerData.Team, "Red")
@@ -81,7 +90,7 @@ func TestParse(t *testing.T) {
 
 			// 7 = changed class
 		case 7:
-			assert.Equal(t, m.Type, PlayerChangedClass)
+			require.Equal(t, m.Type, PlayerChangedClass)
 
 			playerData := m.Data.(PlayerData)
 			assert.Equal(t, playerData.Team, "Blue")
@@ -89,15 +98,64 @@ func TestParse(t *testing.T) {
 
 			// 8 = game over
 		case 8:
-			assert.Equal(t, m.Type, WorldGameOver)
+			require.Equal(t, m.Type, WorldGameOver)
 
 			// 9 = server cvar
 		case 9:
-			assert.Equal(t, m.Type, ServerCvar)
+			require.Equal(t, m.Type, ServerCvar)
 
 			cvarData := m.Data.(CvarData)
 			assert.Equal(t, cvarData.Variable, "sv_password")
 			assert.Equal(t, cvarData.Value, "***PROTECTED***")
+		case 10:
+			require.Equal(t, m.Type, PlayerPickedUpItem)
+
+			pickup := m.Data.(ItemPickup)
+			assert.Equal(t, pickup.Item, "medkit_small")
+			assert.Equal(t, pickup.Healing, 17)
+		case 11:
+			require.Equal(t, m.Type, PlayerKilled)
+			kill := m.Data.(PlayerKill)
+			assert.Equal(t, kill.Weapon, "scattergun")
+		case 12:
+			require.Equal(t, m.Type, PlayerKilled)
+			kill := m.Data.(PlayerKill)
+			assert.Equal(t, kill.CustomKill, "headshot")
+			assert.Equal(t, kill.Weapon, "awper_hand")
+		case 13:
+			require.Equal(t, m.Type, PlayerDamaged)
+			damage := m.Data.(PlayerDamage)
+			assert.Equal(t, damage.Player2, PlayerData{
+				Username: "beastie",
+				UserId:   "5",
+				SteamId:  "[U:1:28701225]",
+				Team:     "Red",
+			})
+			assert.Equal(t, damage.Damage, 100)
+			assert.Equal(t, damage.Weapon, "iron_bomber")
+		case 14:
+			require.Equal(t, m.Type, PlayerDamaged)
+			damage := m.Data.(PlayerDamage)
+			assert.Equal(t, damage.Player2, PlayerData{
+				Username: "≫HarZe",
+				UserId:   "3",
+				SteamId:  "[U:1:40572775]",
+				Team:     "Blue",
+			})
+			assert.Equal(t, damage.Damage, 43)
+			assert.Equal(t, damage.Weapon, "tf_projectile_rocket")
+			assert.True(t, damage.Airshot)
+		case 15:
+			require.Equal(t, m.Type, PlayerHealed)
+			assert.Equal(t, m.Data.(PlayerHeal).Healed, 61)
+		case 16:
+			require.Equal(t, m.Type, PlayerKilledMedic)
+		case 17:
+			require.Equal(t, m.Type, WorldRoundWin)
+			assert.Equal(t, m.Data.(string), "Blue")
+		case 18:
+			require.Equal(t, m.Type, PlayerConnected)
+
 		}
 	}
 }
